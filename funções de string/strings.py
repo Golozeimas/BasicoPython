b255b31983ae88088572dca93e18badc8aa2c771
Matheus+ <joaosobramatheus@gmail.com>
2025-12-17 11:07:12 -0300

    <study> funções de strings no python

---
+++ funções de string/strings.py
@@ -0,0 +1,12 @@
+serieName = "Breaking bad" # 11 posições
+
+print(serieName.upper()) # tudo maiuscula
+print(serieName.lower()) # tudo minuscula
+print(serieName.capitalize()) # primeira letra maiuscula
+print(serieName.title()) # mesmo efeito do capitalize
+
+print(serieName.center(14, "-")) # preenche e centraliza a string
+
+print(serieName.find("d"))
+
+print(serieName.replace("bad", "good")) # substitui o texto pelo o novo
